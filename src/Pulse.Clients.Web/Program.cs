9d9c623621fc4b7c96e6e874294dde9f4e5d7021
Davis Kolakowski <dkolak713@cable.comcast.com>
2025-04-10 14:21:24 -0400

    Update NuGet packages and configuration settings
    - Upgraded several NuGet packages in `Pulse.Api.csproj`, `Pulse.Clients.Web.csproj`, and `Pulse.Core.csproj` for improved compatibility and features.
    - Changed `HttpClient` base address in `Program.cs` to a hardcoded URL for local development.
    - Added `Audience` and `DefaultScopes` fields in `appsettings.Development.json` and `appsettings.json` for Auth0 configuration.
    - Updated `Cronos` package version in `Pulse.Core.csproj` from `0.9.0` to `0.10.0`.

--- src/Pulse.Clients.Web/Program.cs
+++ src/Pulse.Clients.Web/Program.cs
@@ -14,7 +14,11 @@ namespace Pulse.Clients.Web
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient 
+            { 
+                BaseAddress = new Uri("https://localhost:7253") 
+            });
+
             builder.Services.AddFluentUIComponents();
 
             builder.Services.AddAuth0Authentication(options =>
