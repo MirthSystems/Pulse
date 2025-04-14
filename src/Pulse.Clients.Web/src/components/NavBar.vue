5893631ca4b36f71bb3426e7d4b2468d11b8aef8
Davis Kolakowski <dkolak713@cable.comcast.com>
2025-04-14 17:34:22 -0400

    fix: update dependencies and improve NavBar component formatting

--- src/Pulse.Clients.Web/src/components/NavBar.vue
+++ src/Pulse.Clients.Web/src/components/NavBar.vue
@@ -20,9 +20,11 @@
             <li class="nav-item">
               <router-link to="/" class="nav-link">Home</router-link>
             </li>
-            
+
             <li class="nav-item" v-if="isAuthenticated">
-              <router-link to="/external-api" class="nav-link">External API</router-link>
+              <router-link to="/external-api" class="nav-link"
+                >External API</router-link
+              >
             </li>
           </ul>
           <ul class="navbar-nav d-none d-md-block">
@@ -31,7 +33,9 @@
                 id="qsLoginBtn"
                 class="btn btn-primary btn-margin"
                 @click.prevent="login"
-              >Login</button>
+              >
+                Login
+              </button>
             </li>
 
             <li class="nav-item dropdown" v-if="isAuthenticated">
@@ -42,26 +46,43 @@
                 data-toggle="dropdown"
               >
                 <img
-                  :src="user.picture"
+                  :src="user?.picture"
                   alt="User's profile picture"
                   class="nav-user-profile rounded-circle"
                   width="50"
                 />
               </a>
               <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-header">{{ user.name }}</div>
-                <router-link to="/profile" class="dropdown-item dropdown-profile">
+                <div class="dropdown-header">{{ user?.name }}</div>
+                <router-link
+                  to="/profile"
+                  class="dropdown-item dropdown-profile"
+                >
                   <font-awesome-icon class="mr-3" icon="user" />Profile
                 </router-link>
-                <a id="qsLogoutBtn" href="#" class="dropdown-item" @click.prevent="logout">
+                <a
+                  id="qsLogoutBtn"
+                  href="#"
+                  class="dropdown-item"
+                  @click.prevent="logout"
+                >
                   <font-awesome-icon class="mr-3" icon="power-off" />Log out
                 </a>
               </div>
             </li>
           </ul>
 
-          <ul class="navbar-nav d-md-none" v-if="!isAuthenticated && !isLoading">
-            <button id="qsLoginBtn" class="btn btn-primary btn-block" @click="login">Log in</button>
+          <ul
+            class="navbar-nav d-md-none"
+            v-if="!isAuthenticated && !isLoading"
+          >
+            <button
+              id="qsLoginBtn"
+              class="btn btn-primary btn-block"
+              @click="login"
+            >
+              Log in
+            </button>
           </ul>
 
           <ul
@@ -72,12 +93,12 @@
             <li class="nav-item">
               <span class="user-info">
                 <img
-                  :src="user.picture"
+                  :src="user?.picture"
                   alt="User's profile picture"
                   class="nav-user-profile d-inline-block rounded-circle mr-3"
                   width="50"
                 />
-                <h6 class="d-inline-block">{{ user.name }}</h6>
+                <h6 class="d-inline-block">{{ user?.name }}</h6>
               </span>
             </li>
             <li>
@@ -87,7 +108,9 @@
 
             <li>
               <font-awesome-icon icon="power-off" class="mr-3" />
-              <a id="qsLogoutBtn" href="#" class @click.prevent="logout">Log out</a>
+              <a id="qsLogoutBtn" href="#" class @click.prevent="logout"
+                >Log out</a
+              >
             </li>
           </ul>
         </div>
@@ -97,13 +120,14 @@
 </template>
 
 <script lang="ts">
-import { useAuth0 } from '@auth0/auth0-vue';
+import { defineComponent } from "vue";
+import { useAuth0 } from "@auth0/auth0-vue";
 
-export default {
+export default defineComponent({
   name: "NavBar",
   setup() {
     const auth0 = useAuth0();
-    
+
     return {
       isAuthenticated: auth0.isAuthenticated,
       isLoading: auth0.isLoading,
@@ -114,13 +138,13 @@ export default {
       logout() {
         auth0.logout({
           logoutParams: {
-            returnTo: window.location.origin
-          }
+            returnTo: window.location.origin,
+          },
         });
-      }
-    }
-  }
-};
+      },
+    };
+  },
+});
 </script>
 
 <style>
