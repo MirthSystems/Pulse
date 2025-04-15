fd925c616fe601ed3020a0637f6fe04423d114be
Davis Kolakowski <dkolak713@cable.comcast.com>
2025-04-15 18:55:31 -0400

    refactor: update layout structure in default.vue for improved organization and readability

--- src/Pulse.Clients.Web/src/layouts/default.vue
+++ src/Pulse.Clients.Web/src/layouts/default.vue
@@ -1,11 +1,25 @@
 <template>
-  <v-main>
-    <router-view />
-  </v-main>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
 
-  <AppFooter />
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+  import { ref } from 'vue';
+
+  const drawer = ref(false);
 </script>
